bcfabc27557b491b1c7264d30cad3b61a416d7a0
addRian0-0 <addrianun@gmail.com>
2024-11-12 20:49:41 -0600

    solve

--- ConvertidorRestadorBinario/RestadorLED.vhd
+++ ConvertidorRestadorBinario/RestadorLED.vhd
@@ -1,86 +1,49 @@
-library IEEE; 
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 entity RestadorLED is
+            
+    Port ( A : in  STD_LOGIC_VECTOR (2 downto 0);
+           B : in  STD_LOGIC_VECTOR (2 downto 0);
+           
+           salida : out std_logic_vector(16 downto 0));
 
-   Port (
-			A,B: in STD_LOGIC_VECTOR (2 downto 0);
-			DISPLAY: out STD_LOGIC_VECTOR (15 downto 0);
-			RESULTADO : buffer  STD_LOGIC_VECTOR (3 downto 0);
-			BCD : inout STD_LOGIC_VECTOR (3 downto 0);
-			SIGNO : inout STD_LOGIC
-	);
-			
 end RestadorLED;
 
-architecture Behavioral of RestadorLED is 
- 
+architecture Behavioral of RestadorLED is
+    
+    signal diff : STD_LOGIC_VECTOR(3 downto 0);
+    signal final: std_logic_vector(3 downto 0);
 begin
-
-
-process (A, B)
- 
-	  variable diff : STD_LOGIC_VECTOR(3 downto 0);
-begin
-     
-	  diff := ("0" & A) - ("0" & B);
-	  RESULTADO <= diff;
-	  
-	end process;
-
-process(RESULTADO)
+    --Proceso para la resta utilizando diff como ayuda
+    process(A, B)
     begin
-        if RESULTADO(3) = '1' then  -- Si el bit más significativo es 1, es un número negativo
-            SIGNO <= '1';
-            case RESULTADO is
-                when "1001" => BCD <= "0111";  -- -7
-                when "1010" => BCD <= "0110";  -- -6
-                when "1011" => BCD <= "0101";  -- -5
-                when "1100" => BCD <= "0100";  -- -4
-                when "1101" => BCD <= "0011";  -- -3
-                when "1110" => BCD <= "0010";  -- -2
-                when "1111" => BCD <= "0001";  -- -1
-                when others => BCD <= "0000";  -- 0 (en caso de error)
-            end case;
-        else
-            SIGNO <= '0';
-            case RESULTADO is
-                when "0000" => BCD <= "0000";  -- 0
-                when "0001" => BCD <= "0001";  -- 1
-                when "0010" => BCD <= "0010";  -- 2
-                when "0011" => BCD <= "0011";  -- 3
-                when "0100" => BCD <= "0100";  -- 4
-                when "0101" => BCD <= "0101";  -- 5
-                when "0110" => BCD <= "0110";  -- 6
-                when "0111" => BCD <= "0111";  -- 7
-                when others => BCD <= "0000";  -- 0 (en caso de error)
-            end case;
-        end if;
+        diff <= ('0' & A) - ('0' & B);
     end process;
-	 
-process(SIGNO, BCD)
+
+
+    process(diff)
     begin
-        case BCD is
-            when "0000"=> DISPLAY <="0111100001000010";--0
-				when "0001"=> DISPLAY <="0011000000100000";--1
-	         when "0010"=> DISPLAY <="0110100000001010";--2
-	         when "0011"=> DISPLAY <="0111100000001000";--3
-	         when "0100"=> DISPLAY <="0011000001001000";--4
-	         when "0101"=> DISPLAY <="0101100001001000";--5
-	         when "0110"=> DISPLAY <="0101100001001010";--6
-	         when "0111"=> DISPLAY <="0111000000001000";--7
-            when others => DISPLAY <= "0000000000000000";  -- Apagar
+        case(diff) is
+            when "0000" => salida <= "00000000111111111"; -- 0
+				when "0001" => salida <= "11111111101111011"; -- 1
+				when "0010" => salida <= "10010111111101011"; -- 2 
+				when "0011" => salida <= "10000111111101111"; -- 3
+				when "0100" => salida <= "11001111101101111"; -- 4
+				when "0101" => salida <= "10100111101101111"; -- 5
+				when "0110" => salida <= "10100111101101011"; -- 6
+				when "0111" => salida <= "10001111111111111"; -- 7
+				when "1000" => salida <= "00000000111011111"; -- Aqui empiezan los numeros negativos -8
+				when "1001" => salida <= "10001111111011111"; -- -7
+				when "1010" => salida <= "10100111101001011"; -- -6
+				when "1011" => salida <= "10100111101001111"; -- -5
+				when "1100" => salida <= "11001111101001111"; -- -4
+				when "1101" => salida <= "10000111111001111"; -- -3
+				when "1110" => salida <= "10010111111001011"; -- -2
+				when "1111" => salida <= "11001111111011111"; -- -1
+            when others => salida <= "00000000000000001";
         end case;
-
-        -- Mostrar el SIGNO negativo si SIGNO es 1
-        if SIGNO = '1' then
-            DISPLAY(4) <= '1';  -- Activar el segmento superior izquierdo del display como SIGNO "-"
-        else
-            DISPLAY(4) <= '0';  -- Apagar el segmento de SIGNO si el número es positivo
-        end if;
     end process;
-	
 end Behavioral;
--- comparador1/ComparadorBasicoS.vhd
+++ comparador1/ComparadorBasicoS.vhd
@@ -8,7 +8,9 @@ entity ComparadorBasicoS is
         C : in std_logic;   -- Bit C de entrada
         D : in std_logic;   -- Bit D de entrada
         E : in std_logic;   -- Bit E de entrada (como habilitación o control)
-
+		  A1, B2, C3, D4 : in STD_LOGIC; --entradas de 4 bits, porque el nombre tien 8 letras
+		  
+		  salida : out STD_LOGIC_VECTOR (16 downto 0); --A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P
         Ma : out std_logic; -- Salida Ma (N > S)
         Me : out std_logic; -- Salida Me (N < S)
         Ig : out std_logic  -- Salida Ig (N = S)
@@ -16,6 +18,8 @@ entity ComparadorBasicoS is
 end ComparadorBasicoS;
 
 architecture Behavioral of ComparadorBasicoS is
+	--generar una signal para concatenar las varibales
+	signal entrada : STD_LOGIC_VECTOR (3 downto 0);
 begin
     
 	 Ma <= (not A and not B and D and E) or 
@@ -29,5 +33,26 @@ begin
 		(not A and B and not C and D and E) or 
 		(A and B and C and D and E) or 
 		(A and not B and C and not D and E);
+		--concatenamos entradas A1, B2, C3, D4 
+		entrada <= A1 & B2 & C3 & D4;
+			
+			process (entrada)
+			begin
+			
+				case (entrada) is
+					
+					when "0000" => salida <= "00110011110110111";	-- Z
+					when "0001" => salida <= "00001100111001111";	-- A	
+					when "0010" => salida <= "00001100101111011";   -- M 
+					when "0011" => salida <= "00000000111111111";   -- O 
+					when "0100" => salida <= "00011100111001101";   -- R - X
+					when "0101" => salida <= "00110011101111011";	-- I  			
+					when "0110" => salida <= "00111111101111011";	-- T	
+					when "0111" => salida <= "00001100111001111";	-- A	
+					when others => salida <= "11111111111111111";
+					
+				end case;
+				
+			end process;
 	 
 end Behavioral;
